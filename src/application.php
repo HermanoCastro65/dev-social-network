6e1031cda227d485d4427bdcadc2cb4974441eac
Hermano Castro <hermanocastro18@gmail.com>
2023-01-04 19:41:51 -0300

    create setApp method to load urls

--- src/application.php
+++ src/application.php
@@ -4,8 +4,34 @@ namespace src;
 
 class Application
 {
+    private $controller;
+
+    private function setApp()
+    {
+        $loadName = "src\controllers\\";
+        $url = explode('/', @$_GET['url']);
+
+        if ($url[0] == '') {
+            $loadName .= 'Home';
+        } else {
+            $loadName .= ucfirst(strtolower($url[0]));
+        }
+
+        $loadName .= 'Controller';
+
+        if (file_exists($loadName . '.php')) {
+            $this->controller = new $loadName();
+        } else {
+            include("views/pages/404.php");
+            die();
+        }
+
+        $this->controller = new $loadName();
+    }
+
     public function run()
     {
-        echo "runing";
+        $this->setApp();
+        $this->controller->index();
     }
 }
