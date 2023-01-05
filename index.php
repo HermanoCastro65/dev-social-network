327058c6c60dc1a20801f775d8f919e373934980
Hermano Castro <hermanocastro18@gmail.com>
2023-01-04 22:39:59 -0300

    create contant inculde_path

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ session_start();
 require('vendor/autoload.php');
 
 define('INCLUDE_PATH_STATIC', 'http://localhost/dev-social-network/src/views/pages/');
+define('INCLUDE_PATH', 'http://localhost/dev-social-network/');
 
 $app = new src\Application();
 
