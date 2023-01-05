f47dbf0afc8da0d4ee6aac4006ce613f6387f79a
Hermano Castro <hermanocastro18@gmail.com>
2023-01-04 21:43:48 -0300

    create constant include_path_static in index.php and implement it in login page

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ session_start();
 
 require('vendor/autoload.php');
 
+define('INCLUDE_PATH_STATIC', 'http://localhost/dev-social-network/src/views/pages/');
+
 $app = new src\Application();
 
 $app->run();
--- src/views/pages/login.php
+++ src/views/pages/login.php
@@ -1,1 +1,16 @@
-<h2>Login</h2>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <link href="<?php echo INCLUDE_PATH_STATIC ?>styles/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <h2>Olá mundo</h2>
+</body>
+
+</html>
