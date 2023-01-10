c22016bf782b36eef5bbcf6b4746887e221bc105
Hermano Castro <hermanocastro18@gmail.com>
2023-01-10 16:03:45 -0300

    create include folder and put sidebar in a single file

---
+++ src/views/pages/includes/sidebar.php
@@ -0,0 +1,14 @@
+<div class="sidebar">
+	<div class="logo-sidebar">
+		<img src="<?php echo INCLUDE_PATH_STATIC ?>images/logo-hprog.png" />
+	</div>
+	<br />
+	<div class="menu-sidebar">
+		<h4>Menu</h4>
+		<br />
+		<a href="<?php echo INCLUDE_PATH ?>"><i class="fa fa-newspaper-o" aria-hidden="true"></i> feed</a>
+		<a href="#"><i class="fa fa-user-o" aria-hidden="true"></i> perfil</a>
+		<a href="<?php echo INCLUDE_PATH ?>community"><i class="fa fa-users" aria-hidden="true"></i> community</a>
+		<a href="<?php echo INCLUDE_PATH ?>?loggout"><i class="fa fa-sign-out" aria-hidden="true"></i> loggout</a>
+	</div>
+</div>
