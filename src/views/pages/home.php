3be6b37bceb504d4a127ee189873480e690f400a
Hermano Castro <hermanocastro18@gmail.com>
2023-01-10 16:29:53 -0300

    include dynamic sidebar in home

--- src/views/pages/home.php
+++ src/views/pages/home.php
@@ -11,20 +11,9 @@
 
 <body>
     <section class="main-feed">
-        <div class="sidebar">
-            <div class="logo-sidebar">
-                <img src="<?php echo INCLUDE_PATH_STATIC ?>images/logo-hprog.png" />
-            </div>
-            <br />
-            <div class="menu-sidebar">
-                <h4>Menu</h4>
-                <br />
-                <a href="#"><i class="fa fa-newspaper-o" aria-hidden="true"></i> feed</a>
-                <a href="#"><i class="fa fa-user-o" aria-hidden="true"></i> perfil</a>
-                <a href="#"><i class="fa fa-users" aria-hidden="true"></i> friends</a>
-                <a href="?loggout"><i class="fa fa-sign-out" aria-hidden="true"></i> loggout</a>
-            </div>
-        </div>
+        <?php
+        include('includes/sidebar.php');
+        ?>
         <div class="feed">
             <div class="feed-wraper">
                 <div class="feed-single-post">
