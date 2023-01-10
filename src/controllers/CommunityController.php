7f2d04e242ab9d27190e00bfddf84b8bf6444ce4
Hermano Castro <hermanocastro18@gmail.com>
2023-01-10 16:52:56 -0300

    verify if user is logged to see community

--- src/controllers/CommunityController.php
+++ src/controllers/CommunityController.php
@@ -7,6 +7,8 @@ class CommunityController
 
     public function index()
     {
+        if (!isset($_SESSION['login']))
+            \src\Utils::redirect(INCLUDE_PATH);
         \src\views\MainView::render('community');
     }
 }
