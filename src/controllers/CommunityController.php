e93bffd8e9b1be7dd8a99e102d2b4056a36072f8
Hermano Castro <hermanocastro18@gmail.com>
2023-01-10 23:37:53 -0300

    call requestFriendship in CommunityController

--- src/controllers/CommunityController.php
+++ src/controllers/CommunityController.php
@@ -4,11 +4,19 @@ namespace src\controllers;
 
 class CommunityController
 {
-
     public function index()
     {
         if (!isset($_SESSION['login']))
             \src\Utils::redirect(INCLUDE_PATH);
+
+        if (isset($_GET['requestFriendship'])) {
+            $idRequested = (int) $_GET['requestFriendship'];
+            if (\src\models\CommunityModel::requestFriendship($idRequested))
+                \src\Utils::alert("request friendsgip ok");
+            else
+                \src\Utils::alert("error in request friendsgip");
+        }
+
         \src\views\MainView::render('community');
     }
 }
